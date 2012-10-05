59f4d222c7c79d73f4b93faf8e9280059414d15f
Juhani Pelli <juhani.pelli@sc5.io>
2012-10-05 15:58:11 +0300

    Refactored object export scheme; Added MirinItem object; Minification optimizations

--- mirin.js
+++ mirin.js
@@ -1,9 +1,6 @@
-;
-var Mirin;
-
 /* Mirin (Singleton) */
 
-(function(){
+;(function(){
     'use strict';
 
     var options = {
@@ -17,13 +14,15 @@ var Mirin;
         modules = [];
 
     function log() {
-        if (options.debug && window.console && window.console.log) {
-            if ( console.log.apply ) {
-                console.log.apply(console,arguments);
+        var c = window.console;
+        if (options.debug && c && c.log) {
+            if ( c.log.apply ) {
+                c.log.apply(c,arguments);
             } else {
+                // IE
                 var str="";
                 for ( var i in arguments ) str += arguments[i] + " ";
-                console.log(str);
+                c.log(str);
             }
         }
     }
@@ -45,47 +44,8 @@ var Mirin;
         }
     }
 
-    var ElementFactory = {
-        "js" : function(url, module) {
-            var el = document.createElement("script");
-            el.type="text/javascript";
-            return el;            
-        },
-        "css" : function(url, module) {
-            var el = document.createElement("link");
-            el.rel="stylesheet";
-            el.href=url;
-            return el;
-        }
-    };
-
-    function fetchResource(url,callbackFunction) {
-        var xhr = new XMLHttpRequest(),
-            finished=false;
-        xhr.open("GET", url, true);
-        xhr.onload = xhr.onreadystatechange = function(e){
-            var response;
-            try {
-                var response=e.target.responseText;
-            } catch(e) {}
-
-            if ( response && !finished ) {
-                //log("Mirin", "onResourceFetchComplete", url);
-                finished = true;
-                callbackFunction.call(self, response, url);
-            }
-        };
-        xhr.send(null);
-    }
-
-    function onResourcesLoaded(data) {
-        log("Mirin", "onResourcesLoaded");
-        resourceCollection = JSON.parse(data);
-        injectAll();
-    }
-
     /* public api */
-    var self = Mirin = {
+    var self = window.Mirin = {
         init:function(aOptions){
             if ( aOptions ) for ( var i in aOptions ) options[i] = aOptions[i];
             log("Mirin", "init");
@@ -97,7 +57,12 @@ var Mirin;
 
             // possibly fetch collection from url
             if ( options.url ) {
-                fetchResource(options.url,onResourcesLoaded);
+                self.collectionItem = new self.Item(options.url,'json');
+                self.collectionItem.fetch(function(data){
+                    log("Mirin", "loaded resource collection from", options.url);
+                    resourceCollection = JSON.parse(data);
+                    injectAll();
+                });
             }
         },
         inject:function(moduleId, aOptions){
@@ -106,9 +71,7 @@ var Mirin;
             injectAll();
         },
         modules:modules,
-        log:log,
-        ElementFactory:ElementFactory,
-        fetchResource:fetchResource
+        log:log
     };
 }());
 
@@ -156,7 +119,7 @@ var Mirin;
             "js":[],
             "css":[],
             "html":[]
-        }
+        };
         this.creationTime = new Date().getTime();
     };
 
@@ -188,11 +151,61 @@ var Mirin;
         }
     };
 
-    proto.onLoad = function(e,type,url) {
-        log("Mirin loaded", this.id, type, url);
+    proto.inject = function() {
+        var instance = this;
+        for ( var type in this.resources ) {
+            var set = this.resources[type];
+            for ( var i in set ) {
+                var item = new root.Item(set[i], type, this),
+                    el;
+                switch ( type ) {
+                    case "js":
+                        el = document.createElement("script");
+                        el.type="text/javascript";
+                        el.mirinItem = item;
+                        if ( ie ) {
+                            // ie method, see: http://wiki.whatwg.org/wiki/Dynamic_Script_Execution_Order
+                            this.appendQueue.js.push(el);
+                            el.onreadystatechange=onJsReadyStateChange;
+                            el.async=true;
+                            // on IE, fetching starts on ie when src property is set
+                            el.src=item.url;
+                        } else {
+                            el.onload=onJsLoad;
+                            el.async=false;
+                            el.src=item.url;
+                            // others need the element to be appended to start fetch
+                            document.head.appendChild(el);
+                        }
+                        break;
+                    case "css":
+                        el = document.createElement("link");
+                        el.mirinItem = item;
+                        el.rel="stylesheet";
+                        el.href=item.url;
+                        document.head.appendChild(el);
+                        // FIXME: css is marked loaded instanty, since we don't know when it loads
+                        this.onLoad(item);
+                        break;
+                    case "html":
+                        item.fetch(onHtmlLoad);
+                        break;
+                    default:
+                        break;
+                }
+                log("Mirin", "injected", this.id, item.type, item.url);
+            }
+        }
+    };
+
+
+
+    proto.onLoad = function(item) {
+        var url = item.url, type = item.type;
+        log("Mirin loaded", this.id, item.type, url);
         this.loadedResources[type].push(url);
 
-        this.dispatch(EVENTS.progress,e,type,url);
+        this.dispatch(EVENTS.progress,item);
 
         if ( this.setIsLoaded(type) ) {
             log("Mirin completed loading of", type, "set in module", this.id);
@@ -200,71 +213,72 @@ var Mirin;
             while ( setAppendQueue.length > 0 ) {
                 document.body.appendChild(setAppendQueue.shift());
             }
-            this.dispatch(EVENTS.setLoaded,e,type);
+            this.dispatch(EVENTS.setLoaded,item.type);
         }
 
         if ( this.moduleIsLoaded() ) {
             log("Mirin completed loading of module", this.id, "in", new Date().getTime() - this.creationTime, "ms");
-            this.dispatch(EVENTS.moduleLoaded,e);
+            this.dispatch(EVENTS.moduleLoaded);
         }
     };
 
-    proto.inject = function() {
-        for ( var type in this.resources ) {
-            var set = this.resources[type],
-                factoryMethod = root.ElementFactory[type];
-            for ( var i in set ) {
-                var url = set[i];
-                log("Mirin", "injected", this.id, type, url);
-                switch ( type ) {
-                    case "js":
-                        var el = factoryMethod(url, this);
-                        el.originalSrc=url;
-                        var instance = this;
-                        if ( ie ) {
-                            log("Mirin","IE style inject", url);    
-                            // ie method, see: http://wiki.whatwg.org/wiki/Dynamic_Script_Execution_Order
-                            this.appendQueue.js.push(el);
-                            el.onreadystatechange=function(e){
-                                if ( this.readyState=='loaded') {
-                                    log("Mirin",url,"onload");
-                                    instance.onLoad(e,"js",this.originalSrc);
-                                } else if ( this.readyState == "complete") {
-                                    this.onreadystatechange=null;
-                                }
-                            };
-                            el.async=true;
-                            // fetching starts on ie when src property is set
-                            el.src=url; 
-                        } else {
-                            log("Mirin","Webkit style inject", url);    
-                            el.onload=function(e){
-                                log("Mirin",url,"onload");
-                                instance.onLoad(e,"js",this.originalSrc);
-                            };
-                            el.async=false;
-                            el.src=url;  
-                            // others need the element to be appended to start fetch
-                            document.head.appendChild(el);
-                        }
-                        break;
-                    case "css":
-                        document.head.appendChild(factoryMethod(url, this));
-                        // FIXME: css is marked loaded instanty, since we don't know when it loads
-                        this.onLoad(undefined,type,url);
-                        break;
-                    case "html":
-                        var instance = this;   
-                        root.fetchResource(url, function(data, url) {
-                            document.body.innerHTML += data;
-                            instance.onLoad(undefined,"html",url);
-                        });
-                        break;
-                    default:
-                        break;
-                }
+    // HTML load event listener
+    // ensure that instance points to module
+    function onHtmlLoad(data, url) {
+        document.body.innerHTML += data;
+        this.module.onLoad(this);
+    }
+
+    // non-IE JS load event listener
+    // ensure that instance points to module
+    function onJsLoad(e) {
+        this.mirinItem.module.onLoad(this.mirinItem);
+    }
+
+    // IE JS load event listener
+    // ensure that instance points to module
+    function onJsReadyStateChange(e) {
+        if ( this.readyState=='loaded') {
+            this.mirinItem.module.onLoad(this.mirinItem);
+        } else if ( this.readyState == "complete") {
+            this.onreadystatechange=null;
+        }
+    }
+
+}(Mirin));
+
+
+/* Mirin.Item */
+
+(function(root){
+    var MirinItem = function(url,type,module) {
+        this.module = module;
+        this.type = type;
+        this.url = url;
+    };
+    root.Item=MirinItem;
+    var self = MirinItem,
+        proto = self.prototype,
+        log = root.log;
+
+    proto.fetch = function(callbackFunction) {
+        var xhr = new XMLHttpRequest(),
+            finished=false,
+            item=this;
+        xhr.open("GET", this.url, true);
+        xhr.onload = xhr.onreadystatechange = function(e){
+            var response;
+            try {
+                // need to catch error, when xhr is aborted on IE
+                response=e.target.responseText;
+            } catch(ee) {
             }
-        }
-    };
 
+            if ( response && !finished ) {
+                finished = true;
+                callbackFunction.call(item, response, this.url);
+            }
+        };
+        xhr.send(null);
+    };
 }(Mirin));
