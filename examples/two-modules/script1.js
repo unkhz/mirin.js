d639a48a19247236e8b76a6d2e1f2ef4606afc42
Juhani Pelli <juhani.pelli@gmail.com>
2012-10-07 00:29:39 +0300

    Added two-modules example

---
+++ examples/two-modules/script1.js
@@ -0,0 +1,4 @@
+function runFirst() {
+    console.log("First injected script run succesfully");
+    document.getElementById("first").innerHTML += "<p class='inc-first'>script1.js injected</p>";
+}
---
+++ examples/two-modules/script2.js
@@ -0,0 +1,4 @@
+function runSecond(){
+    console.log("Second injected script run succesfully");
+    document.getElementById("second").innerHTML += "<p class='inc-second'>script2.js injected</p>";
+}
