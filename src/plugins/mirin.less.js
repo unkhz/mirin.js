b340049af408c13e3c03d8f2c77da9646eecbb4b
Juhani Pelli <juhani.pelli@sc5.io>
2012-10-10 11:20:37 +0300

    Remove cached CSS before load, since plugin is meant for development use

--- src/plugins/mirin.less.js
+++ src/plugins/mirin.less.js
@@ -1,4 +1,9 @@
-/* less for on-the-fly compiler */
+/* less for less.js on-the-fly compiler 
+ *
+ * USE FOR DEVELOPMENT VERSIONS ONLY
+ *
+ */
+
 (function(){
     MirinResourcePlugin.extend({
         pluginId : "less",
@@ -10,6 +15,18 @@
                     type:"text/css",
                     href:item.url
                 });
+            try {
+                // less.js annoyingly caches the parsed files into localStorage,
+                // which provides no use for development, so we turn it off here
+                var loc = window.location,
+                    keyPrefix = loc.protocol + '//' + loc.location.host + loc.pathname + item.url;
+
+                for (var key in window.localStorage) {
+                    if (key.indexOf(keyPrefix) === 0) {
+                      delete window.localStorage[key];
+                    }
+                }
+            }
             document.head.appendChild(el);
             dispatch(ITEM_EVENTS.inject,item.options,item,item);
             dispatch(ITEM_EVENTS.load,item.options,item,item);
