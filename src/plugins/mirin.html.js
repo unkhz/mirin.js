8ba296bce970da09b1694d2df75afde2d19baea0
Juhani Pelli <juhani.pelli@gmail.com>
2012-10-13 17:27:55 +0300

    Optimized html injection to use appendChild

--- src/plugins/mirin.html.js
+++ src/plugins/mirin.html.js
@@ -8,7 +8,13 @@
             // html-include does not create an element, instead it injects a string
             // which may contain any number of elements or text nodes
             fetch(item.url, function(data) {
-                document.body.innerHTML += data;
+                var i, wrap = document.createElement("div");
+                wrap.innerHTML += data;
+                for ( i = 0; i < wrap.children.length; i++ ) {
+                    var el = wrap.children[i];
+                    // append elements one by one to avoid full page repaint
+                    document.body.appendChild(el);
+                }
                 dispatch(ITEM_EVENTS.inject,item.options,item,item);
                 dispatch(ITEM_EVENTS.load,item.options,item,item);
             });
