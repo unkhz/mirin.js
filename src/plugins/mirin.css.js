1912c82234257209dfccfa96d2ac54c00e3a85f2
Juhani Pelli <juhani.pelli@gmail.com>
2012-10-16 21:49:39 +0300

    Implemented workaround method for CSS onload

--- src/plugins/mirin.css.js
+++ src/plugins/mirin.css.js
@@ -1,18 +1,28 @@
-/* css */
+/* CSS injection with ff-, opera-, chrome- and ie-compatible method 
+ * to determine when loading is finished. This method was described in
+ * http://www.phpied.com/when-is-a-stylesheet-really-loaded/
+ */
 (function(){
     MirinItem.extend({
         pluginId : "css",
         matchExp : /\.css$/i,
         inject: function() {
-            var item = this,
-                el = this.el = extend(createElement("link"), {
-                    rel:"stylesheet",
-                    href:item.url
-                });
+            var dummy,
+                item = this,
+                el = this.el = extend(createElement("style"), {
+                    textContent:'@import "' + item.url + '"'
+                }),
+                loadCheckIntervalId = setInterval(function() {
+                  try {
+                    // error is thrown if resource is not loaded
+                    dummy = el.sheet.cssRules;
+                    item.dispatchLoadEvent();
+                    clearInterval(loadCheckIntervalId);
+                  } catch (ee){}
+                }, 10);
+
             document.head.appendChild(el);
             this.dispatchInjectEvent();
-            // FIXME: CSS files are faked to be loaded immediately, since we cant use onload method
-            this.dispatchLoadEvent();
         }
         
         /*
